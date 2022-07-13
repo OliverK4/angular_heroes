d5f85b70fc133b4b709877d39573c1b96e239d4f
OliverCr <reviloxzxz@gmail.com>
2022-07-13 18:06:30 -0300

    54

---
+++ src/app/hero-powers/hero-powers.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-powers',
+  templateUrl: './hero-powers.component.html',
+  styleUrls: ['./hero-powers.component.css']
+})
+export class HeroPowersComponent implements OnInit {
+
+  @Input() hero? : Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
